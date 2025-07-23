5f95df6a943f5a5180a02f523b09099b9583383a
ocueye2 <carsonmayn@fuse.net>
2025-07-23 15:26:01 -0400

    general improvements

--- gui.py
+++ gui.py
@@ -1,6 +1,7 @@
 from submod.fr import checkuser
-
-from flask import Flask, render_template
+import os
+os.chdir("/home/ocueye/Documents/workspace/docker/vector/vectorlogin")
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -11,7 +12,20 @@ def hello_world():
 @app.route("/vector")
 def vcheck():
     if checkuser():
-         return render_template("suc.html")
+        os.system("killall surf")
+        os.system("hyprland")
+        return render_template("suc.html")
+    else:
+        return render_template("fail.html")
+
+@app.route("/submit",methods=['GET', 'POST'])
+def submit():
+    if request.form["Username"] == "admin":
+        if request.form["password"] == "admin":
+            os.system("/home/ocueye/Documents/workspace/docker/vector/load.sh")
+            return render_template("suc.html")
+        else:
+            return render_template("fail.html")
     else:
         return render_template("fail.html")
 
